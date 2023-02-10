005a532cfe744a06ff2d47ad8579a1c515dbedd3
seojireung0119 <jygarden.tistory.com>
2023-02-10 12:49:49 +0900

    20230210 for문 예제

--- 01_Java/a003_control/src/edu/kh/control/condition/practice/ConditionPracticeRun.java
+++ 01_Java/a003_control/src/edu/kh/control/condition/practice/ConditionPracticeRun.java
@@ -15,11 +15,11 @@ public class ConditionPracticeRun {
 		int num = sc.nextInt();
 		
 		switch(num) {
-		case 1 : practice1(); break;
+		/*case 1 : practice1(); break;
 		case 1 : practice2(); break;
 		case 1 : practice3(); break;
 		case 1 : practice4(); break;
-		case 1 : practice5(); break;
+		case 1 : practice5(); break;*/
 		default : System.out.println("존재하지 않은 예시 번호입니다.");
 		}
 		
