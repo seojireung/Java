2dec55f746ff9505630d6ac0d3c16c33b476afb0
seojireung0119 <jygarden.tistory.com>
2023-02-14 13:35:14 +0900

    20230214 배열, 2차원배열

---
+++ 01_Java/a004_array/src/edu/kh/array/ex/ArrayEx2.java
@@ -0,0 +1,15 @@
+package edu.kh.array.ex;
+
+public class ArrayEx2 {
+
+	// 2차원 배열 사용법 1
+	public void ex1() {
+		
+		// 2차원 배열 선언 및 할당
+		int[][] arr = new int[2][3]; // 2행 3열
+		
+		
+		
+	}
+	
+}
