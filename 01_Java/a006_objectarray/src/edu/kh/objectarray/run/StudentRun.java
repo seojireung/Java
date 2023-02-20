a3ace3968f474fe39c3c9facc71e35425890ec0b
seojireung0119 <jygarden.tistory.com>
2023-02-20 10:50:45 +0900

    20230220_객체 배열

---
+++ 01_Java/a006_objectarray/src/edu/kh/objectarray/run/StudentRun.java
@@ -0,0 +1,13 @@
+package edu.kh.objectarray.run;
+
+import edu.kh.objectarray.view.StudentView;
+
+public class StudentRun {
+	
+	public static void main(String[] args) {
+		
+		StudentView view = new StudentView(); //+import
+		view.displayMenu(); //student view클의 displayMenu 메서드 소환
+	}
+
+}
