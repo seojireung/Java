16b677bbdb4804d2b1b1dd6b1b36d7212eb2159b
seojireung0119 <jygarden.tistory.com>
2023-02-21 10:05:15 +0900

    20230221

--- 01_Java/a006_objectarray/src/edu/kh/objectarray/service/StudentService.java
+++ 01_Java/a006_objectarray/src/edu/kh/objectarray/service/StudentService.java
@@ -1,5 +1,54 @@
 package edu.kh.objectarray.service;
 
+import edu.kh.objectarray.dto.Student;
+
+// 기능 제공용 클래스(비즈니스 로직 처리)
 public class StudentService {
 
+	// ctrl + shift + o : import 자동 추가
+	
+	// Student 객체를 참조하는 변수 5개짜리 배열을 생성해서 studentArr가 참조하게 함
+	private Student[] studentArr = new Student[5];
+	
+	// StudentService 기본 생성자
+	// view 가 만들어지면서 코드가 해석 될 때 아래 요소들도 생성이 됨
+	public StudentService() {
+		
+		// studentArr 배열의 각 요소는 Student 참조 변수
+		studentArr[0] = new Student(3, 5, 17, "홍길동");
+		studentArr[1] = new Student(2, 3, 11, "김철수");
+		studentArr[2] = new Student(1, 7, 3, "최미영");
+	}
+	
+	// param(parameter) : 매개변수
+	
+	/** 1. 학생 추가 서비스
+	 *  @param grade:int
+	 *  @param classRoom:int
+	 *  @param number:int
+	 *  @param name:String
+	 *  
+	 *  @return result : boolean (학생 추가 성공 시 true)
+	 **/
+	public boolean addStudent(int grade, int classRoom, int number, String name) {
+		
+		// studentArr에서 비어있는 인덱스를 찾아
+		// 해당 인덱스에 매개변수를 이용해서 생성된 Student 객체 주소를 대입
+		// -> true 반환
+		for(int i=0; i<studentArr.length; i++) {
+			// 배열 요소가 참조하는 주소가 없을 경우 == 비어있다고 판단
+			if (studentArr[i] == null) {
+				
+			}
+		}
+		
+		// 만약 비어있는 인덱스가 없을 경우
+		// -> false 반환
+		
+		
+		
+		
+		return true; //에러 없애기 위해 true/false 아무거나 씀
+	}
+	
 }
--- 01_Java/a006_objectarray/src/edu/kh/objectarray/view/StudentView.java
+++ 01_Java/a006_objectarray/src/edu/kh/objectarray/view/StudentView.java
@@ -1,9 +1,85 @@
 package edu.kh.objectarray.view;
 
+import java.util.Scanner;
+
+import edu.kh.objectarray.service.StudentService;
+
 public class StudentView {
+	
+	// 필드
+	private Scanner sc = new Scanner(System.in);
+	
+	// 여기서만 사용할 수 있는 StudentService클래스를 참조하는 새 service 변수 선언(생성자 사용)
+	private StudentService service = new StudentService();
 	
 	public void displayMenu() {
 		
+		int input = 0;
+		
+		do {
+			System.out.println("----- [학생 관리 프로그램] -----");
+			System.out.println("1. 학생 추가");
+			System.out.println("2. 학생 전체 조회");
+			System.out.println("3. 학생 정보 조회(인덱스)");
+			System.out.println("4. 학생 정보 조회(이름)");
+			System.out.println("5. 학생 정보 수정(인덱스)");
+			System.out.println("0. 종료");
+			System.out.println("--------------------------------");
+			
+			System.out.print("메뉴 선택 : ");
+			input = sc.nextInt();
+			
+			System.out.println();
+			
+			switch(input) {
+			case 1 : addStudent(); break;
+			case 2 : selectAll(); break;
+			case 3 : selectIndex(); break;
+			case 4 : selectName(); break;
+			case 5 : updateStudent(); break;
+			case 0 : System.out.println("[프로그램이 종료됩니다.]");break;
+			default : System.out.println("[잘못 입력 하셨습니다.]");
+			}
+			
+			
+			System.out.println();
+			
+			
+		}while(input != 0);
+		
 	}
+
+	/** 1. 학생 정보 추가*/
+	private void addStudent() {
+		
+		System.out.println("[학생 추가]");
+		System.out.println("학년 : ");
+		int grade = sc.nextInt();
+		System.out.println("반 : ");
+		int classRoom = sc.nextInt();
+		System.out.println("번호 : ");
+		int number = sc.nextInt();
+		System.out.println("이름 : ");
+		int name = sc.nextInt();
+		
+	}
+
+	private void selectAll() {
+		
+	}
+
+	private void selectIndex() {
+		
+	}
+
+	private void selectName() {
+		
+	}
+
+	private void updateStudent() {
+		
+	}
+
+	
 
 }
