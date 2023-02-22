c22aa61bcaabc44bf417368dc2ac032bbfc545a7
seojireung0119 <jygarden.tistory.com>
2023-02-22 16:27:11 +0900

    20230222_상속

--- 01_Java/a007_inheritance/src/edu/kh/inheritance/dto/Child2.java
+++ 01_Java/a007_inheritance/src/edu/kh/inheritance/dto/Child2.java
@@ -21,9 +21,34 @@ public class Child2 extends Parent{
 		this.house = house;
 	}
 	
+	// 부모의 getMoney() 존재
+	/*
+	 * public int getMoney() {
+		   return money;
+	   }
+	 */
 	
-	public String toString() {
-		return house;
+	// 자식이 상속 받은 getMoney()을 다시 작성(재정의)
+	
+	// @Override : 컴파일러에게 해당 메서드는 재정의 되었다는 것을 알려주는 컴퓨터용 주석
+	//             -> 오버라이딩 형식이 맞는지 검사 진행
+	// 오버라이딩 성립 조건
+	// 메소드 이름 동일
+	// 매개변수의 개수, 타입, 순서 동일
+	// 리턴 타입 동일
+	// private 메소드 오버라이딩 불가
+	// 접근 제어자는 부모와 같거나 넓은 범위로 변경 가능
+	// 예외 처리는 부모와 같거나 좁은 범위로 예외처리 변경 가능
+	
+	// final -> Cannot override the final method from Parent
+	
+	@Override 
+	public int getMoney() {
+	    System.out.println("자식이 오버라이딩한 getMoney");
+	    return super.getMoney() + 500;
+	    // 부모의 getMoney() 반환값에 500원 추가
+	    
+	    
 	}
 	
 }
--- 01_Java/a007_inheritance/src/edu/kh/inheritance/dto/Parent.java
+++ 01_Java/a007_inheritance/src/edu/kh/inheritance/dto/Parent.java
@@ -1,8 +1,11 @@
 package edu.kh.inheritance.dto;
 
-public class Parent {
+// final class : 상속이 불가능한 클래스
+//               보통 코드 재사용을 원치 않을 때 사용
+public class /*final*/ Parent /*extends Object*/ {
+	                // 컴파일러가 자동 추가
 	
-	private int money = 500_000_000; // 돈
+	private int money = 400_000_000; // 돈
 	private String lastName = "박"; // 성
 	
 	// 기본 생성자
@@ -18,7 +21,8 @@ public class Parent {
 	}
 	
 	// getter / setter
-	public int getMoney() {
+	// final 메서드 : 오버라이딩 불가
+	public /*final*/ int getMoney() {
 		return money;
 	}
 
