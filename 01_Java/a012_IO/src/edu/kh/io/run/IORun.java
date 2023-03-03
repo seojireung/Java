04343b3b1c3ef091905142e700374ec02c884db3
seojireung0119 <jiyeong5599@daum.net>
2023-03-03 13:05:08 +0900

    20230303_ IO

---
+++ 01_Java/a012_IO/src/edu/kh/io/run/IORun.java
@@ -0,0 +1,13 @@
+package edu.kh.io.run;
+
+import edu.kh.io.service.IOService;
+
+public class IORun {
+	public static void main(String[] args) {
+		
+		IOService service = new IOService();
+		
+		service.listOutput();
+	}
+
+}
