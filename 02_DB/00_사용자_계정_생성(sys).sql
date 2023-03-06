3a4ebdfa3b8a789015bee6710e7acd37280674f9
seojireung0119 <jiyeong5599@daum.net>
2023-03-06 14:06:06 +0900

    20230306_디비버_사용자계정생성

---
+++ 02_DB/00_사용자_계정_생성(sys).sql
@@ -0,0 +1,22 @@
+-- SQL 한 주 주석
+
+/*
+범위
+주석
+*/
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+-- CTRL + ENTER : 선택한 SQL 수행
+
+-- 사용자 계정 생성
+CREATE USER kh_sjy IDENTIFIED BY "oracle_sjy5599";
+
+-- 사용자 계정에 권한 부여
+GRANT RESOURCE, CONNECT TO kh_sjy;
+
+-- 객체가 생성될 수 있느 ㄴ공간 할당량 지정
+ALTER USER kh_sjy DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+
+
+
