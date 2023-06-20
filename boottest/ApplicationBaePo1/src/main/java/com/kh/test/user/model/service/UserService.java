276e7c661cd4eeb4ad7d633d32a46a4f8d44a048
seojireung <jiyeong5599@daum.net>
2023-06-20 16:29:06 +0900

    20230620_Boot 배포 시험

---
+++ boottest/ApplicationBaePo1/src/main/java/com/kh/test/user/model/service/UserService.java
@@ -0,0 +1,7 @@
+package com.kh.test.user.model.service;
+
+import com.kh.test.user.model.vo.User;
+
+public interface UserService {
+	User searchUser(User userId);
+}
---
+++ boottest/ApplicationBaePo1/src/main/java/com/kh/test/user/model/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.kh.test.user.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.test.user.model.dao.UserMapper;
+import com.kh.test.user.model.vo.User;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserMapper mapper;
+
+	@Override
+	public User searchUser(User userId) {
+		return mapper.searchUser(userId);
+	}
+}
